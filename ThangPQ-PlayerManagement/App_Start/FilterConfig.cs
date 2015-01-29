788aa346d33a87c06b7ee397346a942edb3e9b11
ThangPQ <thangpq@outlook.com.vn>
2015-01-29 22:30:21 +0700

    Initial

---
+++ ThangPQ-PlayerManagement/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ThangPQ_PlayerManagement
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ThangPQ-PlayerManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ThangPQ_PlayerManagement.Startup))]
+namespace ThangPQ_PlayerManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
